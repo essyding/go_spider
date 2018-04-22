05815640573c309333511f2cc6f8c8ebfa9b0b21
Qian Ding <essy.sky@gmail.com>
2018-04-22 18:06:32 -0400

    Rename the sale_page processor and add a new pipeline.

---
+++ custom/sale_page_pipeline.go
@@ -0,0 +1,45 @@
+package custom
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/essyding/go_spider/core/common/com_interfaces"
+	"github.com/essyding/go_spider/core/common/page_items"
+)
+
+type SalePagePipelineFile struct {
+	pFile *os.File
+
+	path string
+
+	listed map[string]bool
+}
+
+func NewSalePagePipelineFile(path string) *SalePagePipelineFile {
+	pFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic("File '" + path + "' in PipelineFile open failed.")
+	}
+	return &SalePagePipelineFile{path: path, pFile: pFile}
+}
+
+func (this *SalePagePipelineFile) Process(items *page_items.PageItems, t com_interfaces.Task) {
+	println("----------------------------------------------------------------------------------------------\n")
+	println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+	for _, value := range items.GetAll() {
+		switch v := value.(type) {
+		case []string:
+			counter := 0
+			for _, vs := range v {
+				if !this.listed[vs] {
+					this.pFile.WriteString(vs)
+					this.listed[vs] = true
+					counter += 1
+				}
+			}
+			fmt.Printf("Crawed %v properties, %v are newly added to db\n", len(v), counter)
+		default:
+		}
+	}
+}
---
+++ custom/sale_page_processor.go
@@ -0,0 +1,87 @@
+package custom
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/essyding/go_spider/core/common/page"
+)
+
+type SalePageProcessor struct {
+}
+
+func NewSalePageProcessor() *SalePageProcessor {
+	return &SalePageProcessor{}
+}
+
+func NormalizeSalePath(s string) string {
+	if !strings.HasPrefix(s, "https://shanghai.anjuke.com/sale") {
+		return ""
+	}
+	u, err := url.Parse(s)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%s://%s%s", u.Scheme, u.Host, u.Path)
+}
+
+func NormalizePropertyPath(s string) string {
+	if !strings.HasPrefix(s, "https://shanghai.anjuke.com/prop/view") {
+		return ""
+	}
+	u, err := url.Parse(s)
+	if err != nil {
+		return ""
+	}
+	return strings.TrimPrefix(u.Path, "/prop/view/")
+}
+
+func ResolveCaptcha(p *page.Page) bool {
+	query := p.GetHtmlParser()
+	if sel := query.Has("div#verify_page"); sel != nil {
+		println("getting captcha...")
+		return true
+	}
+	return false
+}
+
+// Parse html dom here and record the parse result that we want to Page.
+// Package goquery (http://godoc.org/github.com/PuerkitoBio/goquery) is used to parse html.
+func (this *SalePageProcessor) Process(p *page.Page) {
+	// resolve captcha
+	if ResolveCaptcha(p) {
+		p.SetSkip(true)
+		return
+	}
+	query := p.GetHtmlParser()
+	var urls []string
+	query.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		if u := NormalizeSalePath(href); u != "" {
+			urls = append(urls, u)
+		}
+	})
+	// these urls will be saved and crawed by other coroutines.
+	p.AddTargetRequests(urls, "html")
+
+	var props []string
+	query.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		if p := NormalizePropertyPath(href); p != "" {
+			props = append(props, p)
+		}
+	})
+	if len(props) == 0 {
+		println("Seems getting capchaed------------------------\n")
+		println(p.GetBodyStr())
+		p.SetSkip(true)
+	}
+	// the entity we want to save by Pipeline
+	p.AddField("props", props)
+}
+
+func (this *SalePageProcessor) Finish() {
+	fmt.Println("finish")
+}
